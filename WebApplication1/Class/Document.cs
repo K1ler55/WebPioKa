042da247e09a06bb93e423ff0b382ea880c442cd
K1ler55 <piotr.kilarecki@gmail.com>
2017-08-07 14:03:11 +0200

    jestem bogiem

---
+++ WebApplication1/Class/Document.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication1.Class
+{
+    public class Document
+    {
+
+        public virtual int Id_document { get; set; }
+        
+        public virtual string Name { get; set; }
+        public virtual string Permission { get; set; }
+
+        [DataType(DataType.Password)]
+        [Required(ErrorMessage = "This field is required.")]
+        public virtual string Password { get; set; }
+    }
+}
--- WebApplication1/Controllers/AddDocumentController.cs
+++ WebApplication1/Controllers/AddDocumentController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -11,6 +12,20 @@ namespace WebApplication1.Controllers
         // GET: AddDocument
         public ActionResult Index()
         {
+            return View();
+        }
+        public ActionResult Add(HttpPostedFileBase file)
+        {
+            NH.NHibernateOperation operation = new NH.NHibernateOperation();
+            if (file != null)
+            {
+                var filename = Path.GetFileName(file.FileName);
+                var path = Path.Combine(Server.MapPath("~/App_Datta"), filename);
+                file.SaveAs(path);
+                
+                
+            }
+        
             return View();
         }
     }
