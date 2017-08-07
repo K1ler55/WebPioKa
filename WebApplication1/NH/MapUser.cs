e5fb40362b5262f2b8343a204646642cd63c266f
K1ler55 <piotr.kilarecki@gmail.com>
2017-08-07 14:23:47 +0200

    Its Time

--- WebApplication1/NH/MapUser.cs
+++ WebApplication1/NH/MapUser.cs
@@ -12,12 +12,22 @@ namespace WebApplication1
         public MapUser()
         {
 
-            Table("Users123");
+            Table("Users");
             Id(x => x.Id_user, m => { m.Column("id_user"); m.Generator(Generators.Identity); });
             Property(x => x.Name, m => { m.Column("name"); });
             Property(x => x.Surname, m => { m.Column("surname"); });
             Property(x => x.Permission, m => { m.Column("permission"); });
             Property(x => x.Password, m => { m.Column("password"); });
+
+            ManyToOne(x => x.Id_position, m =>
+            {
+                m.Column("id_position");
+            });
+            Bag(x => x.DocumentList, m =>
+            {
+                m.Inverse(true); m.Key(k => k.Column("id_user"));
+
+            }, r => r.OneToMany(x => x.Class(typeof(Step))));
 
         }
     }
