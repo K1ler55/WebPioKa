484f228fb7f0f4f2277d9f4bf489a104c3ed72ea
K1ler55 <piotr.kilarecki@gmail.com>
2017-08-07 09:54:56 +0200

    sfdfsadfas

--- WebApplication1/Class/User.cs
+++ WebApplication1/Class/User.cs
@@ -4,17 +4,23 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace WebApplication1
 {
-    public class User
+    public partial class User
     {
+       
         
-
         public int Id_user { get; set; }
         public string Surname { get; set; }
+        [Display(Name = "Username")]
+        [Required(ErrorMessage ="This field is required.")]
         public string Name { get; set; }
         public string Permission { get; set; }
+
+        [DataType(DataType.Password)]
+        [Required(ErrorMessage = "This field is required.")]
         public string Password { get; set; }
     }
 }
--- WebApplication1/Controllers/LoginController.cs
+++ WebApplication1/Controllers/LoginController.cs
@@ -11,6 +11,18 @@ namespace WebApplication1.Controllers
         // GET: Login
         public ActionResult Index()
         {
+            NH.InitNH init = new NH.InitNH();
+            init.InitNHibernate();
+
+
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Autherize(WebApplication1.User Marcin)
+        {
+             NH.NHibernateOperation operation = new NH.NHibernateOperation();
+            IList<User> lists = operation.GetList<User>();
+            
             return View();
         }
     }
