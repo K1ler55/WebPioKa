1d96f2a12be14ca061486ff16d5f0a5aab4ccd34
K1ler55 <piotr.kilarecki@gmail.com>
2017-08-10 14:01:39 +0200

    tell me whyyyyyy

--- WebApplication1/Controllers/PickFlowController.cs
+++ WebApplication1/Controllers/PickFlowController.cs
@@ -8,23 +8,28 @@ namespace WebApplication1.Controllers
 {
     public class PickFlowController : Controller
     {
-        FlowDefinition flowdefinition =  new FlowDefinition();
+        NH.NHibernateOperation operation = new NH.NHibernateOperation();
+        FlowDefinition flowdefinition = new FlowDefinition();
         Flow flow = new Flow();
+        Position position = new Position();
         public static User user;
         public ActionResult Index()
         {
             if (user == null) return RedirectToAction("Index", "Login");
-            string flowvalue = Request["testSelect"];
-            flowdefinition.id_flowDefinition = Int32.Parse(flowvalue);
-            flow.id_flowdefinition.id_flowDefinition = flowdefinition.id_flowDefinition;
-            WorkEditorController.flowdefinition = flowdefinition;
-            WorkEditorController.flow = flow;
-            return RedirectToAction("Index", "WorkEditor");
-            
+            return View();
         }
         public ActionResult GetFlow()
         {
-           
+            string flowvalue = Request["testSelect"];
+            string flowname =  Request["testFlow"];
+            string positionvalue = Request["PositionSelect"];
+            flowdefinition.id_flowDefinition = Int32.Parse(flowvalue);
+            WorkEditorController.flowdefinition = flowdefinition;
+            flow.id_flowdefinition = flowdefinition;
+            flow.Name = flowname;
+            flow.id_user = user;
+            flow.id_position.Id_position = Int32.Parse(positionvalue);
+            operation.AddElement<Flow>(flow);
             return RedirectToAction("Index", "WorkEditor"); ;
         }
     }
